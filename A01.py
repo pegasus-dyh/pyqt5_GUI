547b18aec49b380dd48eae49f7f3031d580d3ea2
dyh <632189938@qq.com>
2020-07-17 18:38:56 +0800

    new

--- A01.py
+++ A01.py
@@ -1,7 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication,QMainWindow
+from PyQt5.QtWidgets import QApplication,QDesktopWidget,QMainWindow
 from PyQt5.QtGui import QIcon
-import time
 
 class A01(QMainWindow):
     def __init__(self,parent=None):
@@ -11,14 +10,31 @@ class A01(QMainWindow):
         self.setWindowTitle('pegasus的第一个主窗口应用')
 
         #设置窗口尺寸
-        self.resize(400,300)
+        self.resize(500,300)
 
 
         #设置状态栏并显示文字
         self.statusBar().showMessage('这段文字会显示10秒',10000)
 
+
+
+    #窗口居中模块
+        self.center()
+    def center(self):
+        screen=QDesktopWidget().screenGeometry() #获取屏幕坐标
+        size=self.geometry() #获取窗口坐标
+        newleft=(screen.width()-size.width())/2
+        newtop=(screen.height()-size.height())/2
+        self.move(newleft,newtop)
+
+
+
+
+
+
 if __name__=='__main__':
     app=QApplication(sys.argv)
+    app.setWindowIcon(QIcon('./images/Logo.ico'))
     main=A01()
     main.show()
     sys.exit(app.exec_())
