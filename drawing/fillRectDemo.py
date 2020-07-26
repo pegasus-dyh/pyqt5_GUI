78fa2938f72780d0ef2d7f9bef90a0d3350539c1
dyh <632189938@qq.com>
2020-07-26 20:58:14 +0800

    new

---
+++ drawing/fillRectDemo.py
@@ -0,0 +1,51 @@
+'''
+用画刷填充图形区域
+'''
+import sys
+import math
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+class fillRectDemo(QWidget):
+    def __init__(self):
+        super(fillRectDemo, self).__init__()
+        self.resize(600,600)
+        self.setWindowTitle('用画刷填充区域')
+
+    def paintEvent(self,e):
+        painter=QPainter()
+        painter.begin(self)
+        brush=QBrush(Qt.SolidPattern)
+        painter.setBrush(brush)
+        painter.drawRect(10,15,90,60)
+
+        brush=QBrush(Qt.Dense1Pattern)
+        painter.setBrush(brush)
+        painter.drawRect(130,15,90,60)
+
+        brush=QBrush(Qt.Dense2Pattern)
+        painter.setBrush(brush)
+        painter.drawRect(250,15,90,60)
+
+        brush=QBrush(Qt.Dense3Pattern)
+        painter.setBrush(brush)
+        painter.drawRect(10,105,90,60)
+
+        brush=QBrush(Qt.HorPattern)
+        painter.setBrush(brush)
+        painter.drawRect(130,105,90,60)
+
+
+
+
+        painter.end()
+
+
+
+
+if __name__=='__main__':
+    app=QApplication(sys.argv)
+    main=fillRectDemo()
+    main.show()
+    sys.exit(app.exec_())
